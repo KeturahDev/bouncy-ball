9d58336868889dd674112b0d56da99f56c4970f5
Keturah Howard <keturah.dev@gmail.com>
2024-02-02 23:39:33 -0800

    basic render

---
+++ src/components/BouncyBall/BouncyBall.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BouncyBall = () => {
+  return <div>BouncyBall</div>;
+};
+
+export default BouncyBall;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <h1>Bouncy Ball</h1>
+      <h3>I'm just having fun</h3>
+      <hr />
+    </>
+  );
+};
+
+export default Header;
