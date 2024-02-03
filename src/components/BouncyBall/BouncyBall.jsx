8e5cd8539f4c268ad7295aa1d77f9530b1d93fff
Keturah Howard <keturah.dev@gmail.com>
2024-02-03 03:26:11 -0800

    play with additional animations from user events

--- src/components/BouncyBall/BouncyBall.jsx
+++ src/components/BouncyBall/BouncyBall.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import styles from "./styles.module.css";
 
 const BouncyBall = () => {
+  const [clickCount, setClickCount] = React.useState(0);
+  const [ballClasses, setBallClasses] = React.useState(`${styles.ball}`);
+  const ballClick = () => {
+    setBallClasses(`${ballClasses} ${styles.userClicked}`);
+    setClickCount(clickCount + 1);
+  };
+
   return (
     <div className={styles.container}>
-      <div className={styles.ball} />
+      <div>click count: {clickCount} </div>
+      <div onClick={() => ballClick()} className={ballClasses} />
     </div>
   );
 };
