7e9e870486a8971eafa1e9e498166d66f57da9ac
Keturah Howard <keturah.dev@gmail.com>
2024-02-03 02:26:14 -0800

    initial animation for ball

--- src/components/BouncyBall/BouncyBall.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const BouncyBall = () => {
-  return <div>BouncyBall</div>;
-};
-
-export default BouncyBall;
---
+++ src/components/BouncyBall/BouncyBall.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+const BouncyBall = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.ball} />
+    </div>
+  );
+};
+
+export default BouncyBall;
